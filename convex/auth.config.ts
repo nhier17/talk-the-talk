487517b610a58f7b86f4b5ec111aca80f23e9a28
Abraham <abrahamnhier97@gmail.com>
2024-06-19 08:38:47 +0300

    auth config with conex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://cosmic-snipe-26.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
