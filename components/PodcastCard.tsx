870bc1be852ce8fcca123ee6bb5696b3de7fe5bb
Abraham <abrahamnhier97@gmail.com>
2024-06-24 21:37:37 +0300

    podcast details page

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -3,15 +3,22 @@
 import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { PodcastCardProps } from '@/types';
 
 
 const PodcastCard = ({
     imageUrl, title, description, podcastId
-  }) => {
+  }: PodcastCardProps) => {
     const router = useRouter();
 
+    const handleViews = () => {
+      router.push(`/podcast/${podcastId}`, {
+        scroll: true
+      });
+    };
+
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer" onClick={handleViews}>
         <figure className="flex flex-col gap-2">
         <Image 
           src={imageUrl}
