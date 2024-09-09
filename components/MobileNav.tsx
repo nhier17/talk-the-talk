10bd0a8eb7db2e0bb7223fb5b29b4f9783e55d72
Abraham <abrahamnhier97@gmail.com>
2024-09-09 18:18:12 +0300

    mobile responsive

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -27,18 +27,18 @@ const MobileNav = () => {
           <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-black-1">
-          <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 pl-4">
+          <Link href="/" className="flex cursor-pointer items-center gap-1 pb-6 pl-4">
             <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
             <h1 className="text-24 font-extrabold text-white-1 ml-2">Talk the Talk</h1>
           </Link>
-          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+          <div className="flex h-full flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <nav className="flex h-full flex-col gap-6 text-white-1">
+              <nav className="flex flex-col gap-6 text-white-1">
                 {sidebarLinks.map(({ route, label, imgURL }) => {
                   const isActive = pathname === route || pathname.startsWith(`${route}/`);
                   return (
                     <SheetClose asChild key={route}>
-                      <Link href={route} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-start", {
+                      <Link href={route} className={cn("flex gap-3 items-center py-4 px-4", {
                         'bg-nav-focus border-r-4 border-orange-1': isActive
                       })}>
                         <Image src={imgURL} alt={label} width={24} height={24} />
@@ -49,7 +49,7 @@ const MobileNav = () => {
                 })}
               </nav>
             </SheetClose>
-            <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+            <div className="flex-center w-full py-6 px-4">
               <SignedOut>
                 <Button className="text-16 w-full bg-orange-1 font-extrabold text-white-1">
                   <Link href="/sign-in">Sign in</Link>
